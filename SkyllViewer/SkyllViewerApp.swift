c1cc2c50382c48ad12e2216e7d5c8359574f44fc
workingdog <ringowathelet@gmail.com>
2026-05-06 15:14:37 +0900

    Initial Commit

---
+++ SkyllViewer/SkyllViewerApp.swift
@@ -0,0 +1,32 @@
+//
+//  SkyllViewerApp.swift
+//  SkyllViewer
+//
+//  Created by Ringo Wathelet on 2026/05/06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SkyllViewerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
